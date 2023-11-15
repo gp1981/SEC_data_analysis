f57f8f43503d75f5c809d7989ed73a42ab74f7c8
gp1981 <45032495+gp1981@users.noreply.github.com>
2023-11-15 21:35:32 +0100

    New function
    Functions to unnest SEC data structure. Minor updates on reamde

--- code/Functions/data_visualization.R
+++ code/Functions/data_visualization.R
@@ -4,4 +4,20 @@
 
 # Import required libraries
 library(tidyverse)
+library(kableExtra)
 
+# Function to unnest list company_Facts ----------------------------------
+
+# Un-nest the company_Facts (and nested unit list)
+FactsList_to_Dataframe <- function(company_Facts_us_gaap){
+  df_Facts <- company_Facts_us_gaap %>%
+    tibble() %>%
+    unnest_wider(col = everything()) %>%
+    unnest(cols = c(units)) %>%
+    unnest(cols = c(units)) 
+  
+  # Format numbers in the val column as millions
+  df_Facts$val <- scales::number(df_Facts$val / 1e6, accuracy = 0.1, suffix = "M")
+  
+  return(df_Facts)
+}
